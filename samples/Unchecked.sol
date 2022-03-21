09c8bf52203038540dae33c0f8693c57299bb947
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-03-21 22:25:55 +0100

    feat: `unchecked`

---
+++ samples/Unchecked.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity 0.8.11;
+
+// Welcome to the 🧪 Optimizooor's Lab!
+//
+// Get started:
+// 1. Set  Solidity version above
+// 2. Set `optimizer`       in `foundry.toml`
+// 3. Set `optimizer_runs`  in `foundry.toml`
+//
+// Optimize! Run experiments with `forge test`.
+
+// Samples for: Unchecked
+
+contract SharedSetup {
+
+}
+
+// Unoptimized sample
+contract Sample0 is SharedSetup {
+    function measureGas() external {
+        uint zeroToTen;
+        while (zeroToTen < 10) {
+            ++zeroToTen;
+        }
+    }
+}
+
+contract Sample1 is SharedSetup {
+    function measureGas() external {
+        uint zeroToTen;
+        while (zeroToTen < 10) {
+            unchecked {
+                ++zeroToTen;
+            }
+        }
+    }
+}
+
+contract Sample2 is SharedSetup {
+    function measureGas() external {}
+}
+
+contract Sample3 is SharedSetup {
+    function measureGas() external {}
+}
+
+contract Sample4 is SharedSetup {
+    function measureGas() external {}
+}
+
+contract Sample5 is SharedSetup {
+    function measureGas() external {}
+}
+
+// Generated from https://github.com/ZeroEkkusu/optimizooors-lab
+
+// Customize console output
+abstract contract Labels {
+    string label0 = "Checked math";
+    string label1 = "Use `unchecked`";
+    string label2 = "";
+    string label3 = "";
+    string label4 = "";
+    string label5 = "";
+}
